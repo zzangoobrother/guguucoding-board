e4066bba3ef1d5e73f748c4732a5438d128faa91
zzangoobrother <com1368@naver.com>
2022-01-09 02:39:32 +0900

    [feat] 회원, 게시판, 댓글 entity 작성

---
+++ src/main/java/com/example/guguucodingboard/entity/Board.java
@@ -0,0 +1,32 @@
+package com.example.guguucodingboard.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Board extends BaseEntity{
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long bno;
+
+  private String title;
+
+  private String content;
+
+  @ManyToOne
+  private Member writer;
+}
