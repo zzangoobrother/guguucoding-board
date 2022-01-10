87c290a3211a3775cef2a8eefa3c95f37dc46630
zzangoobrother <com1368@naver.com>
2022-01-10 20:50:55 +0900

    [feat] 게시물 조회 화면 작성 및 로직 작성

--- src/main/java/com/example/guguucodingboard/controller/BoardController.java
+++ src/main/java/com/example/guguucodingboard/controller/BoardController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -40,5 +41,13 @@ public class BoardController {
     log.info("BNO : " + bno);
     redirectAttributes.addFlashAttribute("msg", bno);
     return "redirect:/board/list";
+  }
+
+  @GetMapping("/read")
+  public void read(@ModelAttribute("pageRequestDTO") PageRequestDTO pageRequestDTO, Long bno, Model model) {
+    log.info("bno : " + bno);
+    BoardDTO boardDTO = boardService.get(bno);
+    log.info(boardDTO);
+    model.addAttribute("dto", boardDTO);
   }
 }
