cf4744805a3a5586150d0e769e6159101a713e1b
zzangoobrother <com1368@naver.com>
2022-01-10 19:54:19 +0900

    [feat] 게시물 등록 화면 및 로직 구현

--- src/main/java/com/example/guguucodingboard/controller/BoardController.java
+++ src/main/java/com/example/guguucodingboard/controller/BoardController.java
@@ -1,12 +1,15 @@
 package com.example.guguucodingboard.controller;
 
+import com.example.guguucodingboard.dto.BoardDTO;
 import com.example.guguucodingboard.dto.PageRequestDTO;
 import com.example.guguucodingboard.service.BoardService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -23,5 +26,19 @@ public class BoardController {
   public void list(PageRequestDTO pageRequestDTO, Model model) {
     log.info("list................" + pageRequestDTO);
     model.addAttribute("result", boardService.getList(pageRequestDTO));
+  }
+
+  @GetMapping("/register")
+  public void register() {
+    log.info("register get...");
+  }
+
+  @PostMapping("/register")
+  public String registerPost(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
+    log.info("boardDTO..." + boardDTO);
+    Long bno = boardService.register(boardDTO);
+    log.info("BNO : " + bno);
+    redirectAttributes.addFlashAttribute("msg", bno);
+    return "redirect:/board/list";
   }
 }
