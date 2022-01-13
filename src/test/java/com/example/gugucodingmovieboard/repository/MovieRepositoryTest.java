0c2778cdb8c1b60734e979cbbbac41a8c1a60ced
zzangoobrother <com1368@naver.com>
2022-01-13 22:28:02 +0900

    [feat] 영화 페이징 조회 및 특정 영화 조회

--- src/test/java/com/example/gugucodingmovieboard/repository/MovieRepositoryTest.java
+++ src/test/java/com/example/gugucodingmovieboard/repository/MovieRepositoryTest.java
@@ -4,12 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.gugucodingmovieboard.entity.Movie;
 import com.example.gugucodingmovieboard.entity.MovieImage;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 import javax.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.annotation.Commit;
 
 @SpringBootTest
@@ -45,5 +51,26 @@ class MovieRepositoryTest {
       }
       System.out.println("=========================================");
     });
+  }
+
+  @Test
+  void testListPage() {
+    PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Direction.DESC, "mno"));
+
+    Page<Object[]> result = movieRepository.getListPage(pageRequest);
+
+    for (Object[] objects : result.getContent()) {
+      System.out.println(Arrays.toString(objects));
+    }
+  }
+
+  @Test
+  void testGetMovieWithAll() {
+    List<Object[]> result = movieRepository.getMovieWithAll(92L);
+    System.out.println(result);
+
+    for (Object[] arr : result) {
+      System.out.println(Arrays.toString(arr));
+    }
   }
 }
