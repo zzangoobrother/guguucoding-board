3ca78dae72638734dc5fa3f16453ababa09622ae
zzangoobrother <com1368@naver.com>
2022-01-14 23:29:22 +0900

    [feat] 파일 저장 경로 설정

--- src/main/java/com/example/gugucodingmovieboard/controller/UploadController.java
+++ src/main/java/com/example/gugucodingmovieboard/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.example.gugucodingmovieboard.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,6 +9,9 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @Log4j2
 public class UploadController {
+
+  @Value("${com.example.upload.path}")
+  private String uploadPath;
 
   @PostMapping("/uploadAjax")
   public void uploadFile(MultipartFile[] uploadFiles) {
