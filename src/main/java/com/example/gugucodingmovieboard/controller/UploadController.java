cbb08a0408edcd43cd79ac197eaf976518c0390a
zzangoobrother <com1368@naver.com>
2022-01-15 00:39:38 +0900

    [feat] 파일 저장 경로, 중복 이름, 이미지 확장자 고려하여 로직 작성

--- src/main/java/com/example/gugucodingmovieboard/controller/UploadController.java
+++ src/main/java/com/example/gugucodingmovieboard/controller/UploadController.java
@@ -1,5 +1,12 @@
 package com.example.gugucodingmovieboard.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +23,37 @@ public class UploadController {
   @PostMapping("/uploadAjax")
   public void uploadFile(MultipartFile[] uploadFiles) {
     for (MultipartFile uploadFile : uploadFiles) {
+      if (uploadFile.getContentType().startsWith("image") == false) {
+        log.warn("this file is not image type");
+        return;
+      }
+
       String originalName = uploadFile.getOriginalFilename();
       String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
-
       log.info("fileName : " + fileName);
+
+      String folderPath = makeFolder();
+      String uuid = UUID.randomUUID().toString();
+      String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+      Path savePath = Paths.get(saveName);
+
+      try {
+        uploadFile.transferTo(savePath);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+  }
+
+  private String makeFolder() {
+    String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    String folderPath = str.replace("/", File.separator);
+
+    File uploadPathFolder = new File(uploadPath, folderPath);
+    if (uploadPathFolder.exists() == false) {
+      uploadPathFolder.mkdirs();
+    }
+
+    return folderPath;
   }
 }
