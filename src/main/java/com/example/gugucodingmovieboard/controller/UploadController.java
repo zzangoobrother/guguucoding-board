b2427fd3723b818914b61ad8bf158d85198a8016
zzangoobrother <com1368@naver.com>
2022-01-15 10:46:33 +0900

    [feat] 브라우저에 이미지 출력 작성

--- src/main/java/com/example/gugucodingmovieboard/controller/UploadController.java
+++ src/main/java/com/example/gugucodingmovieboard/controller/UploadController.java
@@ -3,6 +3,8 @@ package com.example.gugucodingmovieboard.controller;
 import com.example.gugucodingmovieboard.dto.UploadResultDTO;
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -12,8 +14,11 @@ import java.util.List;
 import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -65,5 +70,27 @@ public class UploadController {
     }
 
     return folderPath;
+  }
+
+  @GetMapping("/display")
+  public ResponseEntity<byte[]> getFile(String fileName) {
+    ResponseEntity<byte[]> result = null;
+
+    try {
+      String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+      log.info("fileName : " + srcFileName);
+
+      File file = new File(uploadPath + File.separator + srcFileName);
+      log.info("file : " + file);
+
+      HttpHeaders headers = new HttpHeaders();
+      headers.add("Content-Type", Files.probeContentType(file.toPath()));
+      result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), headers, HttpStatus.OK);
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    return result;
   }
 }
